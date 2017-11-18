c7b320c4127e909f4c4de38413cfacfb1fe1ec7d
jacklong1979 <lkc311@163.com>
2017-11-18 10:03:06 +0800

    edit

---
+++ ecooljie.Web/Areas/Admin/Scripts/Common.js
@@ -0,0 +1,21 @@
+﻿jQuery.alert = function (message, type) {
+    /// <summary>提示信息</summary>     
+    /// <param name="message" type="String">需要提示的信息</param>        
+    /// <param name="type" type="String">【info】【error】【question】【warning】</param>   
+    if (type == null || type == undefined || type == "") {
+        $.messager.alert('提示信息:', message, 'info');
+    }
+    else if (type == "info") {
+        $.messager.alert('提示信息:', message, 'info');
+    }
+    else if (type == "error") {
+        $.messager.alert('错误信息:', message, 'error');
+    }
+    else if (type == "question") {
+        $.messager.alert('提示信息:', message, 'question');
+    }
+    else if (type == "warning") {
+        $.messager.alert('警告信息:', message, 'warning');
+    }
+
+}
